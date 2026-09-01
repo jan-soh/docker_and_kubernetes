e6a2034420488ee78c92fa08e780dd07925fc894
Jan Sohnemann <jan-so@posteo.de>
2026-09-01 20:26:22 +0200

    exercise results for stage 8

--- create/section_8/multistage-builds/optimizing-images/index.js
+++ create/section_8/multistage-builds/optimizing-images/index.js
@@ -1,1 +1,7 @@
-console.log('Hello world!');
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => res.send('Hello from express!'));
+
+app.listen(3002, () => console.log('listening on port 3002'));
